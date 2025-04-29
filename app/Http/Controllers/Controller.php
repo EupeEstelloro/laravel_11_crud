9bcb2e55e63d1b92607a04d316eadb622a16e9a2
Eupe Estelloro <estelloroeupe@gmail.com>
2025-04-29 12:51:10 +0800

    this is the authentication, but idk know if its right

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,31 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Routing\ControllerMiddlewareOptions;
+
+abstract class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Register middleware on the controller.
+     *
+     * @param  string  $middleware
+     * @param  array  $options
+     * @return \Illuminate\Routing\ControllerMiddlewareOptions
+     */
+    public function middleware($middleware, array $options = [])
+    {
+        foreach ((array) $middleware as $m) {
+            $this->middleware[] = [
+                'middleware' => $m,
+                'options' => &$options
+            ];
+        }
+
+        return new ControllerMiddlewareOptions($options);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\AuthenticationController;
 
+// Public routes
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
+// Authentication routes
+Route::get('/login', [AuthenticationController::class, 'showLogin'])->name('login')->middleware('guest');
+Route::post('/login', [AuthenticationController::class, 'login'])->middleware('guest');
+Route::get('/register', [AuthenticationController::class, 'showRegister'])->name('register')->middleware('guest');
+Route::post('/register', [AuthenticationController::class, 'register'])->middleware('guest');
+Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout')->middleware('auth');
+
+// Protected routes
+Route::middleware('auth')->group(function () {
+    Route::resource('products', ProductController::class);
+});
 
